b291be6ccf109ee4450bb9985b148ad416dfc24b
lucuevas15 <lucuevas.cs@gmail.com>
2025-02-19 17:57:44 -0500

    initial

---
+++ Projects/WelcomeMessages.java
@@ -0,0 +1,12 @@
+public class WelcomeMessages {
+    public static void main(String[] args) {
+        // Output first welcome
+        System.out.println("Welcome to Java");
+        
+        // Output second welcome
+        System.out.println("Welcome to Computer Science");
+        
+        // Output final message
+        System.out.println("Programming is fun");
+    }
+}
