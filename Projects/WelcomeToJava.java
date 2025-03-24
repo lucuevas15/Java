32c79378c85b0f886497af8c3ad44c431cbcf169
lucuevas15 <lucuevas.cs@gmail.com>
2025-03-24 13:03:22 -0400

    initial

---
+++ Projects/WelcomeToJava.java
@@ -0,0 +1,12 @@
+//Java program for 5 prints of "Welcome to Java"//
+
+public class WelcomeToJava {
+    public static void main(String[] args) {
+        int count = 0;
+        //While sttatement to ensure string runs for only 5 prints//
+        while (count < 5) {
+            System.out.println("Welcome to Java");
+            count++;
+        }
+    }
+}
